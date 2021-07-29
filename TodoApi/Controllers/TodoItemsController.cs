4f9fac2bb351cb8dd0205a2f0c1d37af067f4800
jaymanjk <sriraj.jk@gmail.com>
2021-07-29 08:30:16 +0530

    Initial Check - In
    Initial Check - In

---
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() { return "Hello World"; }
+    }
+}
+ 
